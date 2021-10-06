5e56127c7d6e8035273dd3b42c6e05a32b481fe2
Steven Zhang <YZhang@lcs.on.ca>
2021-10-06 09:47:53 -0400

    Initial Commit

---
+++ StaticWeatherView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StaticWeatherView
+//
+//  Created by Steven Zhang on 2021-10-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StaticWeatherView/StaticWeatherViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticWeatherViewApp.swift
+//  StaticWeatherView
+//
+//  Created by Steven Zhang on 2021-10-06.
+//
+
+import SwiftUI
+
+@main
+struct StaticWeatherViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
